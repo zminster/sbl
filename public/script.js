a7621ce1913d75560ecebd38b61d76319cebffe5
Zach Minster <zach.minster@gmail.com>
2021-02-14 03:44:23 -0500

    submission changes

--- public/script.js
+++ public/script.js
@@ -47,45 +47,6 @@ $("#access-rubric").click(function() {
 	});
 });
 
-//responding to rubric
-$("#submit_form").click(function() {
-	// verify 
-
-	var categoryRes1;
-	$("[name='options1']").each(function(i) {
-		if ($(this).is(":checked")) categoryRes1 = i + 1;
-	});
-
-	var categoryRes2;
-	$("[name='options2']").each(function(i) {
-		if ($(this).is(":checked")) categoryRes2 = i + 1;
-	});
-
-	var categoryRes3;
-	$("[name='options3']").each(function(i) {
-		if ($(this).is(":checked")) categoryRes3 = i + 1;
-	});
-
-	var categoryRes4;
-	$("[name='options4']").each(function(i) {
-		if ($(this).is(":checked")) categoryRes4 = i + 1;
-	});
-
-	console.log(
-		categoryRes1 +
-		" " +
-		categoryRes2 +
-		" " +
-		categoryRes3 +
-		" " +
-		categoryRes4
-	);
-
-	// TODO: SEND RESULTS
-
-	location.reload();
-});
-
 $("#exit").click(function() {
 	window.location = "/";
 });
@@ -98,54 +59,5 @@ $("#make-standard").click(function() {
 		standard.find("td:nth-child(" + (i+1) + ") input").attr("name", standard_row + " " + i);
 	}
 	standard.appendTo("#rubric-to-edit tbody");
-	$("#standard_count").val(standard_row);
-});
-
-$("#make-rubric").click(function() {
-
-
-	// TODO: FORM VALIDATION
-
-	// function validateForm() {
-	// 	var isValid = true;
-	// 	$(".form-control").each(function() {
-	// 		if ($(this).val() === "") isValid = false;
-	// 	});
-	// 	$(".chosen-select").each(function() {
-	// 		if ($(this).val() === "") isValid = false;
-	// 	});
-	// 	return isValid;
-	// }
-	//if (validateForm() == true) {
-	var userData = {
-		rubricID: $("#rubricID").val(),
-
-		category1: $("#category1").val(),
-		r1c1: $("#r1c1").val(),
-		r1c2: $("#r1c2").val(),
-		r1c3: $("#r1c3").val(),
-		r1c4: $("#r1c4").val(),
-
-		category2: $("#category2").val(),
-		r2c1: $("#r2c1").val(),
-		r2c2: $("#r2c2").val(),
-		r2c3: $("#r2c3").val(),
-		r2c4: $("#r2c4").val(),
-
-		category3: $("#category3").val(),
-		r3c1: $("#r3c1").val(),
-		r3c2: $("#r3c2").val(),
-		r3c3: $("#r3c3").val(),
-		r3c4: $("#r3c4").val(),
-
-		category4: $("#category4").val(),
-		r4c1: $("#r4c1").val(),
-		r4c2: $("#r4c2").val(),
-		r4c3: $("#r4c3").val(),
-		r4c4: $("#r4c4").val(),
-	};
-	$.post("/addRubric", userData, function(data) {
-		if (data == "alert") alert("You have entered a code that already exists!");
-		else window.location = "/";
-	});
+	$("#standard_count").val(standard_row + 1);
 });
