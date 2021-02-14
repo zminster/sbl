287c52c41907e6e5635bde6d45e80a73ab4edad2
charlie-map <charjhall777@gmail.com>
2021-02-14 02:14:53 -0500

    get rubric and submit rubric complete

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const fs = require('fs'); //enables file access
 const mysql = require('mysql2');
 let mustacheExpress = require("mustache-express");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
@@ -30,7 +32,40 @@ app.get("/", (req, res) => {
 });
 
 app.get("/getRubric/:code", (req, res) => {
-  connection.query("SELECT id FROM ")
+  connection.query("SELECT id, title FROM rubrics WHERE unique_id=?", req.params.code, (err, id) => {
+    if (err) console.log(err);
+    if (id.length) {
+      let obj = {};
+      obj.title = id[0].title;
+      obj.id = id[0].id;
+      obj.standards = {};
+      connection.query("SELECT standard, id, level_one, level_two, level_three, level_four FROM category WHERE rubric_id=?", id[0].id, (err, standards) => {
+        if (err) console.log(err);
+        standards.forEach((item, index) => {
+          obj.standards.push(item);
+        });
+        res.end(obj);
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  });
+});
+
+app.post("/submitRubric", (req, res) => {
+  connection.query("INSERT INTO response (rubric_id) VALUES (?)", req.body.rubric_id, (err) => {
+    if (err) console.log(err);
+    connection.query("SELECT id FROM response WHERE rubric_id=?", req.body.rubric_id, (err, response_id) => {
+      if (err) console.log(err);
+      let choices = req.body.choices;
+      choices.forEach((choice, index) => {
+        connection.query("INSERT INTO results (response_id, category_id, value) VALUES (?, ?, ?)", [response_id[0].id, choice.id, choice.level], (err) => {
+          if (err) console.log(err);
+          res.end();
+        });
+      });
+    });
+  });
 });
 
 app.listen(6969, () => {
